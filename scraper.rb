7002e8cea4ea9ed9c184707790fb81d1ccd4d5f5
Henare Degan <henare.degan@gmail.com>
2017-04-20 10:44:08 +1000

    Override ATDIS gem method to add a 5 minute timeout
    This ATDIS feed takes an excruciatingly long time to respond so we need
    to add an extended timeout or it doesn't work.
    
    I've set the timeout to 5 minutes because in my testing it parsed all 5
    pages of the feed in 5:41 so that should give it lots of room.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 Bundler.require
 
+module ATDIS
+  require "open-uri"
+
+  class Model
+    def self.read_url(url)
+      r = read_json(RestClient::Resource.new(url.to_s, timeout: 300).get.to_str)
+      r.url = url.to_s
+      r
+    end
+  end
+end
+
 url = "http://datracker.begavalley.nsw.gov.au/ATDIS/1.0/"
 
 ATDISPlanningAlertsFeed.save(url)
